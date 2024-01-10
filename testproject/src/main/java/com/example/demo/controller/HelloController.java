3d72b5ffdd10dfcdeb6d024e578aa6d4eab13cec
HEEJUNG <HEEJUNG@DESKTOP-BIC5P49>
2024-01-10 21:28:07 +0900

    스웨거 설정

--- testproject/src/main/java/com/example/demo/controller/HelloController.java
+++ testproject/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,14 +1,13 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-	// @RequestMapping(value = "/hello")
-	@GetMapping("hello")
+	@RequestMapping(value = "/hello")
+	// @GetMapping("hello")
 	public String hello(){
 		return "Hello World";		
 	}
