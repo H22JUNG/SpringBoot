642399869a5a65d7b8538dbab4ca394dd43a9898
heejungkim <heejungkim@wavus.co.kr>
2024-01-15 19:17:02 +0900

    dto 생성

---
+++ testproject/src/main/java/com/example/demo/data/entity/ProductEntity.java
@@ -0,0 +1,13 @@
+package com.example.demo.data.entity;
+
+//@Entity
+public class ProductEntity {
+
+	// pk
+	// @Id
+	String productId;
+	String productName;
+	Integer productPrice;
+	Integer productStock;
+	
+}
