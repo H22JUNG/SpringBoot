fcc9c0ac4ef477a80abbc7305e0e6c8b38aeca8f
H22JUNG <h2jung6875@naver.com>
2024-01-23 21:37:59 +0900

    로거 설정 응용

--- testproject/src/main/java/com/example/demo/controller/HelloController.java
+++ testproject/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,10 +1,21 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	
+	// 로그 설정(org.slf4j.Logger 확인)
+	private final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+	
+	// logback-spring.xml 파일에 들어가는
+	// <pattern>%green([%d{yyyy-MM-dd HH:mm:ss.SSS}]) %magenta([%-5level]) %highlight([%thread]) %cyan(%logger{30}) %yellow(%msg%n)</pattern>
+	// 에서 %logger에 getLogger(클래스명.class)의 클래스명이 들어감
+
 
 	@RequestMapping(value = "/hello")
 	// @GetMapping("hello")
@@ -12,5 +23,15 @@ public class HelloController {
 
 		return "hello world";		
 	}
+	
+	@PostMapping("/log-test")
+	public void logTest() {
+		LOGGER.trace("Trace Log");
+		LOGGER.debug("Debug Log");
+		LOGGER.info("Info Log");
+		LOGGER.warn("Warn Log");
+		LOGGER.error("Error Log");
+	}
+	
 
 }
--- testproject/src/main/java/com/example/demo/controller/ProductController.java
+++ testproject/src/main/java/com/example/demo/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,7 @@ import com.example.demo.service.ProductService;
 @RequestMapping("/api/v1/product-api")
 public class ProductController {
 	
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
 	private ProductService productService;
 	
 	@Autowired	// 위에서 선언한 service 객체를 new()로 생성하지 않아도, 아래에서 쓸 수 있도록 자동 연결
@@ -26,7 +29,19 @@ public class ProductController {
 	@GetMapping(value = "/product/{productId}")
 	public ProductDto getProduct(@PathVariable String productId) {
 		
-		return productService.getProduct(productId);
+		long startTime = System.currentTimeMillis();
+		LOGGER.info("[ProductController] perform {} of test API.", "getProduct");
+		// 결과:[INFO ][http-nio-8080-exec-1]c.e.d.c.ProductController[ProductController] perform getProduct of test API.
+		// {}로 결과값을 넣을 자리를 설정해주고, " ," 후에 차례대로 들어갈 값을 설정해주면 차례로 출력됨
+		
+		ProductDto productDto = productService.getProduct(productId);
+
+		// 로그 응용
+		LOGGER.info("[ProductController] Response :: productId = {}, productName = {}, productPrice = {}, productStock = {}, Response Time = {}ms",
+				productDto.getProductId(), productDto.getProductName(), productDto.getProductPrice(), productDto.getProductStock(),
+				(System.currentTimeMillis() - startTime));
+		
+		return productDto;
 	}
 	
 	 @PostMapping("/product")
